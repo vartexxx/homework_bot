b74124417523f933f8ddc1c2faa1e674fa7f2ea6
vartex <vartexxx29@yandex.ru>
2023-01-13 00:41:01 +0500

    fix: исправления после 1-го ревью

--- homework.py
+++ homework.py
@@ -7,6 +7,7 @@ import requests
 from http import HTTPStatus
 from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
+from requests import RequestException
 
 from exceptions import (
     SendMessageError,
@@ -21,6 +22,7 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+GLOBAL_VARIABLES = ('PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
@@ -51,26 +53,30 @@ formatter = logging.Formatter(
 handler.setFormatter(formatter)
 
 
-def check_tokens():
+def check_tokens() -> None:
     """Функция проверяет наличие перменных окружения."""
     logger.info('Проверка наличия переменных окружения')
-    if([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID].count(None) == 0):
-        return True
+    for value in GLOBAL_VARIABLES:
+        if globals()[value] is None:
+            logger.critical(f'Пременная окружения: {value} отсутствует.')
+            raise MissingTokensError(f'Пременная окружения: {value} отсутствует.')
 
 
-def send_message(bot, message):
+def send_message(bot: telegram.Bot, message: str) -> None:
     """Функция отправляет сообщениие через бота в чат."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.debug(
             f'Сообщение отправлено в чат {TELEGRAM_CHAT_ID}: {message}'
         )
-    except Exception:
-        logger.error('Ошибка отправки сообщения')
+    except telegram.error.TelegramError as error:
+        logger.error(
+            f'Ошибка - {error} при отправке сообщения: {message}'
+        )
         raise SendMessageError('Ошибка отправки сообщения')
 
 
-def get_api_answer(timestamp):
+def get_api_answer(timestamp: int) -> dict:
     """Функция выполняет запрос к единственному ENDPOINT API."""
     playload = {'from_date': timestamp}
     try:
@@ -79,35 +85,42 @@ def get_api_answer(timestamp):
             headers=HEADERS,
             params=playload
         )
-    except Exception as error:
+    except RequestException as error:
         logger.error(f'Ошибка при запросе API: {error}')
         raise HomeworkEndpointError(f'Ошибка при запросе API: {error}')
-    if homework_status.status_code == HTTPStatus.OK:
-        return homework_status.json()
-    else:
+    if homework_status.status_code != HTTPStatus.OK:
         raise ResponseError('API вернул статус отличный от 200')
+    return homework_status.json()
 
 
-def check_response(response):
+def check_response(response: dict) -> list:
     """Проверка ответа запроса от API."""
-    try:
-        homework_list = response['homeworks']
-    except KeyError as error:
-        logger.error(f'Ошибка доступа по ключу \'homeworks\': {error}')
+    if not isinstance(response, dict):
+        logger.error(f'Ответ запроса - не словарь. {type(response)}')
+        raise TypeError(f'Ответ запроса - не словарь. {type(response)}')
+    if 'homeworks' not in response:
+        logger.error(f'Ошибка доступа по ключу \'homeworks\':')
+        raise KeyError('Ошибка доступа по ключу \'homeworks\'')
+    homework_list = response['homeworks']
     if not isinstance(homework_list, list):
-        logger.error('Homeworks представлены не в виде списка')
+        logger.error(
+            f'Homeworks представлены не в виде списка: {type(homework_list)}'
+        )
         raise TypeError('Homeworks представлены не в виде списка')
     return homework_list
 
 
-def parse_status(homework):
+def parse_status(homework: dict) -> str:
     """Функция проверяет наличие статуса домашней работы в ответе."""
-    try:
-        homework_name = homework['homework_name']
-    except KeyError:
-        logger.error('Неверный ответ сервера')
+    if 'homework_name' not in homework:
+        logger.error('Неверный ответ сервера по ключу \'homework_name\'')
+        raise KeyError('Неверный ответ сервера по ключу \'homework_name\'')
+    if 'status' not in homework:
+        logger.error('Неверный ответ сервера по ключу \'status\'')
+        raise KeyError('Неверный ответ сервера по ключу \'status\'')
+    homework_name = homework['homework_name']
     homework_status = homework['status']
-    if(homework_status not in HOMEWORK_VERDICTS) or (homework_status == ''):
+    if(homework_status not in HOMEWORK_VERDICTS) or not homework_status:
         logger.error(f'Статус работы неккоректен: {homework_status}')
         raise KeyError(f'Статус работы неккоректен: {homework_status}')
     verdict = HOMEWORK_VERDICTS[homework_status]
@@ -116,11 +129,7 @@ def parse_status(homework):
 
 def main():
     """Основная логика работы бота."""
-    if not check_tokens():
-        logger.critical('Отсутствуют необходимые пременные окружения')
-        raise MissingTokensError(
-            'Отсутствуют необходимые переменные окружения'
-        )
+    check_tokens()
     timestamp = int(time.time())
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     last_message = ''
